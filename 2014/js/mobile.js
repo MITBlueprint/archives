e4746a440544aa9014de977822ad4d4a1fe8f7ed
Edwin Zhang <ehzhang@mit.edu>
2014-12-24 20:59:55 -0500

    more relative

--- 2014/js/mobile.js
+++ 2014/js/mobile.js
@@ -9,7 +9,7 @@ if ($(window).width() < 650) {
 	// slide menu
 	$('head').append('<link rel="stylesheet" hrep="/css/snap.css">');
 	$('#container').prepend('<div id="slidebutton"><i class="fa fa-bars"></i></div>');
-	$('body').append('<script src="/js/snap.min.js"></script>');
+	$('body').append('<script src="js/snap.min.js"></script>');
 	$('body').append('<script>var snapper = new Snap({element: document.getElementById("container"), disable: "right", maxPosition: 175});document.getElementById("slidebutton").addEventListener("click", function(){if( snapper.state().state==="closed" ){ snapper.open("left"); } else {snapper.close();}});</script>');
 }
 
